223563f44c2fd8aab4bddb62a523bb2d3c19e923
sdtemple <sdtemple@uw.edu>
2025-02-19 19:20:19 -0800

    playing around with conv nn

--- nn-selcoef/simple_selcoef.py
+++ nn-selcoef/simple_selcoef.py
@@ -2,8 +2,10 @@
 # github.com/sdtemple/isweep 
 
 import numpy as np
+import torch
 from scipy.stats import binom
 from math import floor, ceil, sqrt
+import matplotlib.pyplot as plt
 
 def read_Ne(file):
     '''Read *.ne file
@@ -196,3 +198,93 @@ def walk_variant_backward(s, p0, Ne, random_walk = False, one_step_model = 'a', 
             ps.append(p)
 
         return np.array([np.array(ps), np.array(Ns), np.array(xs)]) # numpy-ify
+    
+def bootstrap(num_rep: int,
+              s_start: float,
+              s_step: float,
+              s_end: float,
+              p_start: float,
+              p_step: float,
+              p_end : float,
+              Ne: dict,
+              gens: list,
+              sizes: list
+              ):
+    ss = np.arange(s_start,s_end,s_step)
+    N = num_rep
+    ps = np.arange(p_start,p_end,p_step)
+    M = N*len(ss)*len(ps)
+    y_array = np.zeros((M,1),dtype=np.float32)
+    x_array = np.zeros((M,gens.shape[0]),dtype=np.float32)
+    j = 0
+    for p in ps:
+        for s in ss:
+            for n in range(N):
+                y = s
+                x0, _, _ = walk_variant_backward(y,p,Ne,random_walk=True,one_step_model='m') # wright fisher process
+                # address fixation of allele
+                last_element = x0[-1] # Get the last element of the array
+                extension = np.full(gens[-1], last_element) # Create an array of the same value
+                extended_arr = np.append(x0, extension) # Extend the original array
+                x = extended_arr[gens]
+                x_sampled = np.random.binomial(sizes,x) / sizes
+                x_array[j,:] = x_sampled
+                y_array[j] = y
+                j += 1
+    x_data = torch.from_numpy(x_array)
+    y_data = torch.from_numpy(y_array)
+    return x_data, y_data
+
+def bootstrap_interval(model,
+                       nboot: int,
+                       prediction: float,
+                       p: float,
+                       Ne: dict,
+                       gens: list,
+                       sizes: list,
+                       qlow=0.025,
+                       qupp=0.975,
+                       ) -> list:
+    x_data, _ = bootstrap(nboot,
+                          prediction,
+                          0.011,
+                          prediction+0.01,
+                          p,
+                          0.11,
+                          p+0.1,
+                          Ne,
+                          gens,
+                          sizes,
+                          )
+    y_boot = model(x_data)
+    y_boot = y_boot.cpu().detach().numpy()
+    ylow = np.quantile(y_boot, qlow)
+    yupp = np.quantile(y_boot, qupp)
+    return ylow, yupp
+
+def sweep(s: float,p: float,Ne: dict,gens: list):
+    x0, _, _ = walk_variant_backward(s,p,Ne,random_walk=False,one_step_model='m') # wright fisher process
+    last_element = x0[-1] # Get the last element of the array
+    extension = np.full(gens[-1], last_element) # Create an array of the same value
+    extended_arr = np.append(x0, extension) # Extend the original array
+    x = extended_arr[gens]
+    return x[1:]
+
+def plot_scatter_pred(predicted, actual, downsample_prop=1.):
+    # Convert tensors to numpy arrays
+    predicted_np = predicted.cpu().detach().numpy()
+    actual_np = actual.cpu().detach().numpy()
+    nrows = predicted.shape[0]
+    nkeep = int(nrows*downsample_prop)
+    sampled_indices = np.random.choice(nrows,nkeep,replace=False)
+    predicted_np_down = predicted_np[sampled_indices]
+    actual_np_down = actual_np[sampled_indices]
+
+    # Plot the data
+    plt.scatter(actual_np_down, predicted_np_down, alpha=0.5)
+    plt.plot([actual_np.min(), actual_np.max()], [actual_np.min(), actual_np.max()], 'r--', label='Ideal Fit')
+    plt.xlabel('Actual values')
+    plt.ylabel('Predicted values')
+    plt.grid(alpha=0.25)
+    plt.legend()
+    plt.show()
